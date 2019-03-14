06079747bc67be29611859e941d3028ff2010b19
bbocz <benboczulak@outlook.com>
2019-03-14 15:57:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
-navBarToggle.addEventListener("click",function() {
-  mainNav.classList.toggle('active');
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
 });
